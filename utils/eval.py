ffd36f7f910c425123295a5269d16b7f36610c9f
samuele-bortolato <samuele.bortolato@gmail.com>
2022-11-04 19:28:00 +0100

    add visualization of reconstruction sequence

--- utils/eval.py
+++ utils/eval.py
@@ -1,26 +1,9 @@
 import os
 import cv2
-import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-from utils.reconstruction import reconstruct_sequentially, reconstruct_single_step, reconstruct_step
-
-
-def load_weights(net : torch.nn.Module, checkpoint_filename : str):
-    """Load weights from a checkpoint. The model is automatically set to eval mode.
-
-    Parameters
-    ----------
-    net : torch.nn.Module
-    checkpoint_filename : str
-    """
-    if not os.path.exists(checkpoint_filename):
-        raise FileNotFoundError(f"The file {checkpoint_filename} does not exist.")
-
-    checkpoint = torch.load(checkpoint_filename)
-    net.load_state_dict(checkpoint['model_state_dict'])
-    net.eval()
+from utils.reconstruction import reconstruct
 
 
 def _visualize_images(original_image,
@@ -53,6 +36,8 @@ def _visualize_images(original_image,
     axs[2].set_title('Bilateral filter')
 
     for i, im in enumerate(rec_step):
+        if hasattr(im, "__getitem__"):
+            im=im[-1]
         axs[i+3].imshow(im.transpose(1, 2, 0))
         axs[i+3].set_title(f'step = {step[i]}')
 
@@ -64,42 +49,52 @@ def _visualize_images(original_image,
 
 
 def _visualize_images_plotly(original_image,
-                      noisy_image,
-                      rec_baseline,
-                      rec_step,
-                      step,
-                      t=None):
+                             noisy_image,
+                             rec_baseline,
+                             rec_step,
+                             step,
+                             t=None):
     import plotly.express as px
 
-    images=np.concatenate([ noisy_image.transpose(1, 2, 0)[None],
-                            original_image.transpose(1, 2, 0)[None],                 
-                            rec_baseline.transpose(1, 2, 0)[None]
-                            ],0)
+    images=np.concatenate([(noisy_image.transpose(1, 2, 0).clip(0, 1)*255).astype(np.uint8)[None],
+                           (original_image.transpose(1, 2, 0).clip(0, 1)*255).astype(np.uint8)[None],                 
+                           (rec_baseline.transpose(1, 2, 0).clip(0, 1)*255).astype(np.uint8)[None]
+                          ],
+                          axis=0)
 
-    labels=['noisy','original','baseline']
+    labels=['noisy', 'original', 'baseline']
 
     for i, im in enumerate(rec_step):
-        images=np.concatenate([ images,
-                                im.transpose(1, 2, 0)[None]
-                                ],0)
+        images=np.concatenate([images,
+                               im.transpose(1, 2, 0)[None]
+                              ],
+                              axis=0)
         labels.append(f'step = {step[i]}')
 
-    fig = px.imshow(images, facet_col=0,  facet_col_spacing=0.005, facet_row_spacing=0.05, width=1000,height=1000, facet_col_wrap=2)
+    nrows = 1 + int(np.ceil((len(rec_step)+1)/2))
+    ncols = 2
+    w, h, dpi = 1200, int(1200*nrows/ncols), 100
+
+    print(w, h)
+
+    fig = px.imshow(images, facet_col=0,  facet_col_spacing=0.005, facet_row_spacing=0.05, width=w, height=h, facet_col_wrap=2)
     fig.for_each_annotation(lambda a: a.update(text=labels[int(a.text.split("=")[-1])]))
     fig.show()
 
 
-def visualize_single_reconstruction(net,
-                                    original_image,
-                                    noisy_image,
-                                    t,
-                                    variance_schedule,
-                                    device=None,
-                                    sigma=None,
-                                    step=1,
-                                    output_folder=None,
-                                    bilateral_parameters=(2, 0.7),
-                                    use_plotly=False):
+def reconstruct_and_compare(net,
+                            original_image,
+                            noisy_image,
+                            t,
+                            variance_schedule,
+                            device=None,
+                            sigma=None,
+                            step_size=1,
+                            overstep=0.5,
+                            output_folder=None,
+                            bilateral_parameters=(2, 0.7),
+                            return_sequences=True,
+                            use_plotly=False):
     """Visualize a single reconstruction of an image with sequential and single step reconstruction.
 
     Parameters
@@ -113,21 +108,34 @@ def visualize_single_reconstruction(net,
     t : int
         timestep
     variance_schedule : array or array-like
-        sequence for beta_t
+        sequence for beta_t.
     device : string, optional
     sigma : {int, array or array-like}, optional
         noise sequence to add at each sequential reconstruction step.
-    step : int, optional
-        skip step in step reconstruction.
+    step_size : int, optional
+        the number of original timesteps equivalent to the reconstruct timestep.
+    overstep : float, optional
+        correction factor to the predicted noise. For more info, see Stable diffusion paper.
+        The default is 0.5.
     output_folder : str, optional
         if provided, save the original image, noisy image and reconstructinos output_folder.
-    bilateral_parameter : tuple(float, float), optional
+    bilateral_parameters : tuple(float, float), optional
         set the parameters for the baseline bilateral filtering. The elements are
         ``(sigmaSpace, sigmaColor)``. Default is (2, 0.7).
+    return_sequences : bool, optional
+        if set to False, returns the reconstructed images for each step_size.
+        If set to True, returns the entire reconstruction sequence for each step_size.
+        Default is True.
+    use_plotly : bool, optional
+        Choose whether to use ``plotly`` or ``matplotlib``. Default is False.
+
+    Returns
+    3D numpy array (or 4D numpy array if ``return_sequences`` is set to True)
+        reconstructed image with shape ``(n_channels, spatial_1, spatial_2)``.    
     """
 
-    if not hasattr(step, "__getitem__"):
-        step = [step]
+    if not hasattr(step_size, "__getitem__"):
+        step_size = [step_size]
 
     rec_baseline = cv2.bilateralFilter(np.asarray(noisy_image, dtype=np.float32).transpose(1, 2, 0),
                                                   d=-1,
@@ -135,8 +143,8 @@ def visualize_single_reconstruction(net,
                                                   sigmaColor=bilateral_parameters[1])
 
     rec_step = []
-    for s in step:
-        rec_step.append(reconstruct_step(net, noisy_image, t, s, variance_schedule, device))
+    for s in step_size:
+        rec_step.append(reconstruct(net, noisy_image, t, variance_schedule, s, overstep=overstep, sigma=sigma, return_sequences=return_sequences, device=device))
 
     if output_folder is not None:
         if not os.path.exists(output_folder):
@@ -148,20 +156,32 @@ def visualize_single_reconstruction(net,
         plt.imsave(os.path.join(output_folder, 'baseline.png'), rec_baseline)
         print('baseline saved')
 
-        for s, im in zip(step, rec_step):
-            plt.imsave(os.path.join(output_folder, f'rec_step_{s}.png'), im.transpose(1, 2, 0))
+        for s, im in zip(step_size, rec_step):
+            if return_sequences:
+                plt.imsave(os.path.join(output_folder, f'rec_step_{s}.png'), im[-1].transpose(1, 2, 0))
+            else:
+                plt.imsave(os.path.join(output_folder, f'rec_step_{s}.png'), im.transpose(1, 2, 0))
             print(f'step {s} saved')
+
     if use_plotly:
         _visualize_images_plotly(original_image=original_image,
                       noisy_image=noisy_image,
                       rec_baseline=rec_baseline.transpose(2, 0, 1),
-                      rec_step=rec_step,
-                      step=step,
+                      rec_step=[r[-1] for r in rec_step] if return_sequences else rec_step,
+                      step=step_size,
                       t=t)
     else:
         _visualize_images(original_image=original_image,
                       noisy_image=noisy_image,
                       rec_baseline=rec_baseline.transpose(2, 0, 1),
-                      rec_step=rec_step,
-                      step=step,
+                      rec_step=[r[-1] for r in rec_step] if return_sequences else rec_step,
+                      step=step_size,
                       t=t)
+    
+    return [r.transpose(0, 2, 3, 1) for r in rec_step] if return_sequences else rec_step.transpose(1, 2, 0)
+
+
+def visualize_reconstruction_sequence(rec_images):
+    import plotly.express as px
+    fig = px.imshow(rec_images, animation_frame=0, width=1000,height=1000)
+    fig.show()
