371fdcca42e878cf6f90947a894121d1d52e37f8
michele98 <calvanese.michele@gmail.com>
2023-01-20 18:45:39 +0100

    improve docs

--- utils/encodings.py
+++ utils/encodings.py
@@ -2,16 +2,22 @@ import torch
 
 
 def pos_encoding(t, channels, spatial_dimensions, device='cpu'):
-    """Positional encoding of the timestep.
-    Each channels is a component of the fourier series of t.
+    """Positional encoding of the timestep. For a more detailed explanation:
+    https://kazemnejad.com/blog/transformer_architecture_positional_encoding/
+
+    We take the first `channels` components of the positional encoding.
+
+    The integer timestep is transformed into a tensor of size `(channels, spatial_dimensions)`.
+    The first half of channels contains the sine components, and the second half the cosine components.
+    The values of the positional encoding are constant along the spatial dimensions and change along the channels.
 
     Parameters
     ----------
     t : Tensor
-        timestep. Its shape must be (1,).
+        timestep. Its shape must be `(batch_size, 1)`.
     channels : int
-        corresponds to the number of fourier components of the series.
-        Half the channels are the sin components and half for the cos  components. Must be an even number.
+        corresponds to the number of components of the series.
+        Half the channels are the sin components and half for the cos components. Must be an even number.
     spatial_dimensions : tuple of int
     device : str, optional
         by default 'cpu'.
@@ -22,7 +28,6 @@ def pos_encoding(t, channels, spatial_dimensions, device='cpu'):
         positional encoding of t with shape
         ``(batch_size, channels, spatial_dimensions[0], spatial_dimensions[1])``
     """
-    #TODO: check that 10000!
     inv_freq = 1.0 / (
         10000
         ** (torch.arange(0, channels, 2, device=device).float() / channels)
